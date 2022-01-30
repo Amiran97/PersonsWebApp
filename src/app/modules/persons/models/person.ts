098c163c186f84f3ba4547293c8c4776da99ef65
Amiran <todua.amiran97@gmail.com>
2022-01-31 01:10:30 +0200

    Create module, services, models for persons

---
+++ src/app/modules/persons/models/person.ts
@@ -0,0 +1,9 @@
+export interface Person {
+    id: string,
+    firstName: string,
+    lastName: string,
+    phone: string,
+    email: string,
+    birthday: string,
+    modifyDate: string
+}
---
+++ src/app/modules/persons/services/persons-facade.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Person } from '../models/person';
+import { PersonsApiService } from './persons-api.service';
+import { PersonsStorageService } from './persons-storage.service';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class PersonFacadeService {
+    constructor(
+        private personsApi: PersonsApiService,
+        private personsStorage: PersonsStorageService) { 
+          this.load();
+      }
+    
+      get persons$() {
+        return this.personsStorage.persons$;
+      }
+    
+      create(person: Person) : Observable<Person> {
+        return this.personsApi.create(person).pipe(
+          tap(data => this.personsStorage.create(data)));
+      }
+    
+      remove(id: string) : Observable<void> {
+        return this.personsApi.remove(id).pipe(
+          tap(() => this.personsStorage.remove(id)));
+      }
+
+      update(id: string, person: Person) : Observable<Person> {
+          return this.personsApi.update(id, person).pipe(
+              tap(() => this.personsStorage.update(id, person)));
+      }
+    
+      private load() {
+        this.personsApi.getAll()
+          .subscribe(data => this.personsStorage.set(data));
+      }
+}
